2d67bb2a536119dff6144605308e8bc1ed6d2926
goongrh <82572469+goongrh@users.noreply.github.com>
2024-05-29 10:12:20 +0700

    Create yum-install-docker.sh

---
+++ yum-install-docker.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+#add docker-ce repo
+sudo yum config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+
+#install docker-ce
+sudo yum install -y docker-ce
+
+#start & enable docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+#check
+sudo systemctl is-active docker
+docker --version
+
+#add docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64" -o /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
+docker-compose version
